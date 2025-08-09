56f392d792a6b24a2707b82216a302d2af25880b
AreedaTJ <142763951+AreedaTJN@users.noreply.github.com>
2025-08-09 18:00:19 +0700

    edit script

--- q2_pipeline_db/sql/create_tables.sql
+++ q2_pipeline_db/sql/create_tables.sql
@@ -17,3 +17,29 @@ CREATE TABLE IF NOT EXISTS articles (
     doc_type TEXT,
     source_id INTEGER REFERENCES sources(source_id)
 );
+
+-- ตรวจสอบสิทธิ์ user
+SELECT grantee, privilege_type
+FROM information_schema.role_table_grants
+WHERE table_catalog = 'research_db';
+
+-- หรือเช็คสิทธิ์การเชื่อมต่อ
+SELECT rolname, rolcanlogin FROM pg_roles;
+
+GRANT CONNECT ON DATABASE research_db TO "AreedaTJ";
+
+SELECT rolname, rolcanlogin FROM pg_roles WHERE rolname = 'AreedaTJ';
+
+ALTER ROLE "AreedaTJ" WITH LOGIN;
+
+SELECT rolname, rolcanlogin FROM pg_roles WHERE rolname = 'AreedaTJ';
+
+GRANT SELECT, INSERT, UPDATE, DELETE ON TABLE articles TO "AreedaTJ";
+
+GRANT USAGE, SELECT ON SEQUENCE articles_article_id_seq TO "AreedaTJ";
+
+-- คำสั่งรวม
+-- GRANT CONNECT ON DATABASE research_db TO "AreedaTJ";
+-- GRANT USAGE ON SCHEMA public TO "AreedaTJ";
+-- GRANT SELECT, INSERT, UPDATE, DELETE ON TABLE articles TO "AreedaTJ";
+-- GRANT USAGE, SELECT ON SEQUENCE articles_article_id_seq TO "AreedaTJ";
